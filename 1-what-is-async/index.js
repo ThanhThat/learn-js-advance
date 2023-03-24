41b94dbff49ce32cf650f20723713cf9c8a92eee
ThanhThat <thachthanhthat1995@gmail.com>
2023-03-24 15:48:41 +0700

    Successfully course!

---
+++ 1-what-is-async/index.js
@@ -0,0 +1,9 @@
+// Asynchronous => là bất đồng bộ
+// synchronous => đồng bộ
+
+/* **
+*      bát đồng bộ trong js là gì giúp js xử lý code nhanh hơn, 
+*  dòng nào chạy trước được thì js cho chạy trước. Gọi Api tốn thời gian,
+   bất đồng bộ phải ra tay để đảm bảo dữ liệu trả về trước, để có dữ liệu cho các
+   tiến trình sao xử lý.
+*/
